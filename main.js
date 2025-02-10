d9b0e666d0f14bf14a41d3c5b8401eaba3c4007a
Maria <mariadrodriguez.va@gmail.com>
2025-02-10 12:55:15 -0700

    add external numbers to test using a card number generator -uncomment original variable with the name \"batch"\ for testing all code

--- main.js
+++ main.js
@@ -19,9 +19,15 @@ const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3]
 const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3]
 const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3]
 
+const test1 = [3, 6, 8, 8, 4, 0, 1, 9, 3, 0, 9, 7, 1, 0];
+const test2 = [6, 3, 7, 8, 4, 8, 3, 0, 6, 1, 6, 5, 1, 9, 3, 0];
+const test3 = [6, 0, 1, 1, 7, 6, 9, 1, 9, 7, 2, 5, 0, 8, 6, 7];
+const test4 = [2, 2, 2, 1, 0, 0, 4, 6, 7, 0, 7, 5, 7, 5, 7, 5];
+const test5 = [4, 5, 5, 6, 3, 7, 0, 6, 5, 0, 1, 4, 5, 4, 8, 2];
+
 // An array of all the arrays above
 
-const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5]
+const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5, test1, test2, test3, test4, test5]
 // Add your functions below:
 
 // reverse index nested in batch indexes
