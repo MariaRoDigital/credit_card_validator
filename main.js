0f83650109450f427fc2cb53f2e3ad51ba501505
Maria <mariadrodriguez.va@gmail.com>
2025-03-03 15:48:52 -0700

    create invalid to valid function -add console logs to help check function output in certain areas -comment out specific functions to test invalid to valid function using node

--- main.js
+++ main.js
@@ -96,12 +96,13 @@ const findInvalidCards = arr => {
     if (digitsSum % 10 !== 0) {
       invalidCards.push(batchIndex);
     }
+    console.log(digitsSum)
   }
   return invalidCards;
 }
 
 const invalidCards = findInvalidCards(batch);
-// console.log(invalidCards);
+console.log(invalidCards);
 
 // IDENTIFY INVALID CARD ISSUING COMPANIES
 const idInvalidCardCompanies = arr => {
@@ -126,7 +127,8 @@ const idInvalidCardCompanies = arr => {
   console.log(filteredCompanies);
 }
 
-idInvalidCardCompanies(invalidCards);
+// idInvalidCardCompanies(invalidCards);
+
 
 // extra challenge: tested using card numbers from a card number generator and validator site
 
@@ -142,6 +144,111 @@ const stringToArray = str => {
   return convertedString;
 }
 
-console.log(stringToArray(string));
+// console.log(stringToArray(string));
 
+// extra challenge: create a function that will convert invalid numbers into valid numbers
+const invalidToValid = arr => {
+  let newValidCards = [];
+  let numSum;
 
+  for (let index of arr) {
+    let revArr = index.toReversed();
+    let digitCheckResult = [];
+
+    // iterates through the digits in the each individual array inside of batch, leaves every other number alone but does add it to the array called digitCheckResult, doubles any number that isn't more than 9 when doubled and adds it to digitCheckResult, or when the number is more than 9 when doubled, it multiplies the digit by 2 and then subtracts 9 from the result, then adds it to digitCheckResult.
+    for (let checkDigit = 0; checkDigit < revArr.length; ++checkDigit) {
+      if (checkDigit === 0 || checkDigit % 2 === 0) {
+        digitCheckResult.push(revArr[checkDigit]);
+      } else if (revArr[checkDigit] * 2 > 9) {
+        digitCheckResult.push((revArr[checkDigit]) * 2 - 9);
+      } else if (revArr[checkDigit] * 2 <= 8) {
+        digitCheckResult.push(revArr[checkDigit] * 2)
+      }
+    }
+
+    // creates a variable with the sum of the digit results
+    let digitsSum = digitCheckResult.reduce((acc, currentValue) => acc + currentValue);
+
+    // looks at the sum of digitsSum divided by 10, looks at the remainder and then uses the remainder result to check the number (51, 52, 53 and so on) to change one of the digits into a value that will give the sum a valid number
+
+    // replace sum when sum has a remainder of 1
+    if (digitsSum % 10 === 1) {
+      if (revArr.indexOf(5) && revArr.indexOf(5) !== revArr[0]) {
+        revArr.splice(revArr.indexOf(5), 1, 0)
+      } else {
+        revArr.splice(revArr.indexOf(1), 1, 4)
+      }
+
+      // replace sum when sum has a remainder of 2
+    } else if (digitsSum % 10 === 2) {
+      if (revArr.indexOf(1) && revArr.indexOf(1) !== revArr[0]) {
+        revArr.splice(revArr.indexOf(1), 1, 0)
+      } else if (revArr.indexOf(2) && revArr.indexOf(2) !== revArr[0]) {
+        revArr.splice(revArr.indexOf(2), 1, 1)
+        revArr.splice(revArr.indexOf(1), 1, 0)
+      }
+
+      // replace sum when sum has a remainder of 3
+    } else if (digitsSum % 10 === 3) {
+      if (revArr.indexOf(6) && revArr.indexOf(6) !== revArr[0]) {
+        revArr.splice(revArr.indexOf(6), 1, 0)
+      } else if (revArr.indexOf(2) && revArr.indexOf(2) !== revArr[0]) {
+        revArr.splice(revArr.indexOf(2), 1, 1)
+        revArr.splice(revArr.indexOf(1), 1, 5)
+      }
+
+      // replace sum when sum has a remainder of 4
+    } else if (digitsSum % 10 === 4) {
+      if (revArr.indexOf(6) && revArr.indexOf(6) !== revArr[0]) {
+        revArr.splice(revArr.indexOf(6), 1, 0)
+      } else if (revArr.indexOf(2) && revArr.indexOf(2) !== revArr[0]) {
+        revArr.splice(revArr.indexOf(2), 1, 1)
+        revArr.splice(revArr.indexOf(1), 1, 5)
+      }
+
+      // replace sum when sum has a remainder of 5
+    } else if (digitsSum % 10 === 5) {
+      if (revArr.indexOf(7) && revArr.indexOf(7) !== revArr[0]) {
+        revArr.splice(revArr.indexOf(7), 1, 0)
+      } else if (revArr.indexOf(1) && revArr.indexOf(1) !== revArr[0]) {
+        revArr.splice(revArr.indexOf(1), 1, 2)
+      }
+
+      // replace sum when sum has a remainder of 6
+    } else if (digitsSum % 10 === 6) {
+      if (revArr.indexOf(3) && revArr.indexOf(3) !== revArr[0]) {
+        revArr.splice(revArr.indexOf(3), 1, 0)
+      } else if (revArr.indexOf(7) && revArr.indexOf(7) !== revArr[0]) {
+        revArr.splice(revArr.indexOf(7), 1, 9)
+      }
+
+      // replace sum when sum has a remainder of 7
+    } else if (digitsSum % 10 === 7) {
+      if (revArr.indexOf(0) && revArr.indexOf(0) !== revArr[0]) {
+        revArr.splice(revArr.indexOf(0), 1, 6)
+      } else if (revArr.indexOf(5) && revArr.indexOf(5) !== revArr[0]) {
+        revArr.splice(revArr.indexOf(5), 1, 2)
+      }
+
+      // replace sum when sum has a remainder of 8
+    } else if (digitsSum % 10 === 8) {
+      if (revArr.indexOf(1) && revArr.indexOf(1) !== revArr[0]) {
+        revArr.splice(revArr.indexOf(1), 1, 2)
+      } else if (revArr.indexOf(4) && revArr.indexOf(4) !== revArr[0]) {
+        revArr.splice(revArr.indexOf(4), 1, 0)
+      }
+
+      // replace sum when sum has a remainder of 9
+    } else if (digitsSum % 10 === 9) {
+      if (revArr.indexOf(0) && revArr.indexOf(0) !== revArr[0]) {
+        revArr.splice(revArr.indexOf(0), 1, 5)
+      } else if (revArr.indexOf(9) && revArr.indexOf(9) !== revArr[0]) {
+        revArr.splice(revArr.indexOf(9), 1, 0)
+      }
+    }
+    newValidCards.push(revArr.toReversed())
+  };
+  return newValidCards
+}
+
+console.log(findInvalidCards(invalidToValid(invalidCards)))
